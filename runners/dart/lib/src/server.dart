4ec942b3a3cad6a63cea884e5253da7372490f6c
Derrick Liu <derrick56007@hotmail.com>
2022-08-10 00:14:06 +0000

    changes

--- runners/dart/lib/src/server.dart
+++ runners/dart/lib/src/server.dart
@@ -1,3 +1,19 @@
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+
 import 'tank.dart';
 
-void run(BaseTank bot) async {}
+void run(BaseTank bot) async {
+  var app = Router();
+
+  app.get('/hello', (Request request) {
+    return Response.ok('hello-world');
+  });
+
+  app.get('/user/<user>', (Request request, String user) {
+    return Response.ok('hello $user');
+  });
+
+  await io.serve(app, 'localhost', 8080);
+}
