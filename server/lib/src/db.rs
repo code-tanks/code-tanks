c9f8b8e17a5db1ed8994205620aed15c7838f9f8
Derrick Liu <derrick56007@hotmail.com>
2022-08-12 18:25:23 +0000

    base36

--- server/lib/src/db.rs
+++ server/lib/src/db.rs
@@ -15,28 +15,20 @@ pub fn get_db_pool() -> Pool<PostgresConnectionManager<NoTls>> {
         /* https://gist.github.com/david-sanabria/0d3ff67eb56d2750502aed4186d6a4a7 */
         CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-        CREATE OR REPLACE FUNCTION base62_encode( long_number bigint ) 
+        CREATE OR REPLACE FUNCTION base36_encode( long_number bigint ) 
         RETURNS text
         AS $BODY$
         /*
-        * base62_encode()
+        * base36_encode()
         *
         * This function accepts a small or big number (base 10) and reduces its length into a string
-        * that is URI-safe using the upper and lower case 26-letter English alphabet 
-        * as well as the numbers 0 - 9. The result is returned as a text string that can be decoded
-        * based to base10 using the base62_decode() function.
-        *
-        * You can find a handy explainer at https://helloacm.com/base62/
-        *
-        *
-        * HISTORY
-        * 2018-03-13 david sanabria, office of systems integration
-        *            - New function
+        * that is URI-safe using the lower case 26-letter English alphabet 
+        * as well as the numbers 0 - 9. The result is returned as a text string.
         *
         */
         declare
-            k_base        constant integer := 62;
-            k_alphabet    constant text[] := string_to_array( '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'::text, null);
+            k_base        constant integer := 36;
+            k_alphabet    constant text[] := string_to_array( '0123456789abcdefghijklmnopqrstuvwxyz'::text, null);
             
             v_return_text text := '';
             v_remainder   integer;
@@ -58,69 +50,6 @@ pub fn get_db_pool() -> Pool<PostgresConnectionManager<NoTls>> {
         
         
             return v_return_text;
-        
-        end;$BODY$
-        LANGUAGE plpgsql
-        immutable		    /* Makes no changes to data in tables */
-        returns null ON NULL INPUT  /* Don't bother to call if the value is NULL */
-        SECURITY INVOKER            /* No reason to use DEFINER for security */
-        cost 5                      /* A made up number. Any advice? */
-        ;
-        
-        CREATE OR REPLACE FUNCTION base62_decode( encoded_text text ) 
-        RETURNS bigint
-        AS $BODY$
-        /*
-        * base62_decode()
-        *
-        * This function accepts a string that has been base62 encoded. Any characters that are not valid
-        * are simply ignored, so you can safely pass a formatted string and still get a valid result like you
-        * do with a UUID field.
-        *
-        * HISTORY
-        * 2018-03-13 david sanabria, office of systems integration
-        *            - New function
-        *
-        */
-        declare
-            k_base          constant integer := 36;
-            k_alphabet      constant text := '0123456789abcdefghijklmnopqrstuvwxyz';
-            
-            v_encoded_arr   text[];
-            v_return_result bigint   := 0;
-            v_interim	    bigint;
-            v_index         integer;  -- Pointer to input array
-            v_token         text;
-            v_power         integer := 0;  -- reverse pointer, used for position exponent (e.g. 2^32)
-        begin
-        
-            -- check for guard values
-            if encoded_text is null or length( encoded_text ) = 0 then
-            return null;
-            end if;
-        
-            -- reverse the input string to make the exponent math simpler below
-            v_encoded_arr := string_to_array( reverse( encoded_text ) , null );
-        
-        
-            --Conversion Loop
-            foreach v_token in array v_encoded_arr
-            loop
-        
-            v_index := strpos( k_alphabet, v_token );
-        
-            if v_index = 0 then
-                raise notice 'Token ignored "%"', v_token;
-                --ignore invalid tokens, which allows formatted strings to be processed (e.g. '{abc-1Lg}')
-            else
-                    v_return_result := v_return_result + ( ( v_index - 1) *  pow( k_base, v_power) );
-                v_power := 1 + v_power; --increment after each valid loop
-                end if;
-        
-            end loop;
-        
-        
-            return v_return_result;
         
         end;$BODY$
         LANGUAGE plpgsql
@@ -162,7 +91,7 @@ pub fn insert_tank(
                     SELECT ENCODE(DIGEST($1 || $2,'sha256'), 'hex') AS id
                 )
                 INSERT INTO tanks (id, url, code, status)
-                SELECT id, SUBSTRING(base62_encode(('x'||lpad(id,16,'0'))::bit(64)::bigint), 0, 8), $1, 'queued'
+                SELECT id, SUBSTRING(base36_encode(('x'||lpad(id,16,'0'))::bit(64)::bigint), 0, 8), $1, 'queued'
                 FROM cte;
             ",
             &[&code_as_json_string, &post_fix],
