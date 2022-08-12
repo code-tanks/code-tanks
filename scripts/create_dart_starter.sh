70cc265cd4f4c74f15f627bd7258ca2ca478a876
Derrick Liu <derrick56007@hotmail.com>
2022-08-12 21:51:14 +0000

    names

---
+++ scripts/create_dart_starter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git clone -b dart-template --single-branch https://github.com/derrick56007/codetanks.git $1
--- scripts/get_dart_starter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-git clone -b dart-template --single-branch https://github.com/derrick56007/codetanks.git dart_tanks
