9a13b3eb4614d0658957d4d7cb6e5721a017c024
Derrick Liu <derrick56007@hotmail.com>
2022-08-16 22:06:50 +0000

    d

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@
 git pull
 
 docker compose up -d db ocypod ocypod-redis registry builder pgAdmin
-docker compose up -d --build --force-recreate --remove-orphans server builder
+docker compose up -d --build --force-recreate --remove-orphans server builder simulator
