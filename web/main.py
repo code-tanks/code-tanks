19591436cc8bb91e75639fc728ea3b0f9835a81f
Derrick Liu <derrick56007@hotmail.com>
2022-08-18 23:20:25 +0000

    t

--- web/main.py
+++ web/main.py
@@ -5,9 +5,21 @@ from fastapi.responses import FileResponse
 app = FastAPI()
 
 @app.get("/ping")
-async def root():
+def root():
     return "pong"
 
 @app.get('/')
 def index():
   return FileResponse('/ctweb/web/index.html')
+
+@app.get('/ctviewer.js')
+def f1():
+  return FileResponse('/ctweb/web/ctviewer.js')
+
+@app.get('/ctviewer_bg.wasm')
+def f2():
+  return FileResponse('/ctweb/web/ctviewer_bg.wasm')
+
+@app.get('/ctviewer_bg.wasm.d.ts')
+def f3():
+  return FileResponse('/ctweb/web/ctviewer_bg.wasm.d.ts')
