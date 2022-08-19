6170a1a774b1912738a44cf487bfe7860bd6a3ea
Derrick Liu <derrick56007@hotmail.com>
2022-08-19 19:04:44 +0000

    c

--- web/main.py
+++ web/main.py
@@ -59,7 +59,8 @@ def f4(game_id: str):
     game_id = "/".join(game_id.split(".")[0].split("-"))
 
     print(game_id)
-    
-    r = requests.get(f'http://server:8089/sim/{game_id}')
+    session = requests.Session()
+    session.trust_env = False
+    r = session.get(f'http://server:8089/sim/{game_id}')
 
     return Response(content=r.text, media_type="text/plain")
