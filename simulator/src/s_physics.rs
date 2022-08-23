56b611428249ed08d8c809d626163e2644fe1dc0
Derrick Liu <derrick56007@hotmail.com>
2022-08-23 21:13:15 +0000

    register hit events

--- simulator/src/s_physics.rs
+++ simulator/src/s_physics.rs
@@ -11,21 +11,8 @@ pub fn physics(
     rapier_context: Res<RapierContext>,
     query_bullet: Query<Entity, With<Bullet>>,
     query_collidable: Query<Entity, (With<Collider>, Without<Bullet>)>,
-    // query: Query<(Entity, &Health, Option<&EventSink>, Option<&Bullet>)>,
     mut commands: Commands,
 ) {
-    // for collision_event in collision_events.iter() {
-    //     info!("{:?}", collision_event);
-    //     for (entity, health, event_sink, bullet) in query.iter() {
-    //         if let CollisionEvent::Started(a, b, _event_flag) = collision_event {
-    //             if a == &entity {
-    //                 check(a, health, event_sink, bullet, &mut commands);
-    //             } else if b == &entity {
-    //                 check(b, health, event_sink, bullet, &mut commands);
-    //             }
-    //         }
-    //     }
-    // }
     for a in query_collidable.iter() {
         for bullet in query_bullet.iter() {
             /* Find the intersection pair, if it exists, between two colliders. */
@@ -33,12 +20,6 @@ pub fn physics(
                 commands.entity(bullet).despawn();
             }
         }
-        // for (b, b_transform, b_health, b_event_sink) in query_collidable.iter() {
-        //     if rapier_context.intersection_pair(a, b) == Some(true) {
-        //         hit(a, b, a_transform, b_transform, a_health, a_event_sink);
-        //         hit(b, a, b_transform, a_transform, b_health, b_event_sink);
-        //     }
-        // }
     }
 }
 
@@ -64,27 +45,3 @@ pub fn physics2(
         }
     }
 }
-
-// fn hit(
-//     a: Entity,
-//     _b: Entity,
-//     a_transform: &Transform,
-//     _b_transform: &Transform,
-//     _health: Option<&Health>,
-//     _event_sink: Option<&EventSink>,
-// ) {
-//     info!("HIT {:?} {:?}", a, a_transform);
-// }
-
-// fn check(
-//     entity: &Entity,
-//     _health: &Health,
-//     event_sink: Option<&EventSink>,
-//     bullet: Option<&Bullet>,
-//     commands: &mut Commands,
-// ) {
-//     if let Some(_bullet) = bullet {
-//         commands.entity(*entity).despawn();
-//     } else if let Some(_event_sink) = event_sink {
-//     }
-// }
