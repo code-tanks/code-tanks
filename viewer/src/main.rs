07dd380e437d5989de3de6087882fd8b41674d66
Derrick Liu <derrick56007@hotmail.com>
2022-08-24 21:49:45 +0000

    t

--- viewer/src/main.rs
+++ viewer/src/main.rs
@@ -16,6 +16,7 @@ use s_tanks::*;
 
 fn main() {
     App::new()
+        .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .init_resource::<CState>()
         .add_asset::<CustomAsset>()
