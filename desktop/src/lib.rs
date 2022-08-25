abafd75afa8fde930fc70bbd39b8bc90d3fb2f07
Derrick Liu <derrick56007@hotmail.com>
2022-08-25 03:51:51 +0000

    run docker image

--- desktop/src/lib.rs
+++ desktop/src/lib.rs
@@ -1,3 +1,5 @@
+use std::process::Command;
+
 use bevy::prelude::*;
 use bevy_prototype_lyon::prelude::*;
 use bevy_rapier2d::prelude::*;
@@ -19,7 +21,57 @@ pub mod s_request_debug_commands;
 pub mod s_setup_tanks;
 pub mod s_update_health;
 
-pub fn run_game(tank_ids: &[String]) {
+pub fn run_tank(url: &str, game_url: &str, post_fix: usize) -> String {
+    // docker run -d --network=codetanks_default -p  8080:8080 --name tank_id --label com.docker.compose.project=codetanks localhost:5001/url
+    let tank_id = format!("{}-{}-{}", game_url, url, post_fix);
+    let output_raw = Command::new("docker")
+        .arg("run")
+        .arg("-d")
+        .arg("--network=codetanks_default")
+        .arg("-p")
+        .arg("8080")
+        .arg("--name")
+        .arg(&tank_id)
+        .arg("--label")
+        .arg("com.docker.compose.project=codetanks")
+        .arg(format!("localhost:5001/{}", url))
+        .output()
+        .expect("failed to communicate with docker");
+    let result_raw = String::from_utf8_lossy(&output_raw.stdout);
+    // let err_raw = String::from_utf8_lossy(&output_raw.stderr);
+
+    println!("run stdout:");
+    println!("{}", result_raw.to_string());
+
+    // let output_raw = Command::new("bash")
+    //     .arg("-c")
+    //     .arg(format!(r#"docker port {} 8080 | cut -d: -f2"#, tank_id))
+    //     .output()
+    //     .expect("failed to communicate with docker");
+    // let result_raw = String::from_utf8_lossy(&output_raw.stdout);
+    // // let err_raw = String::from_utf8_lossy(&output_raw.stderr);
+    // let port = result_raw
+    //     .to_string()
+    //     .split("\n")
+    //     .map(|f| f.to_string())
+    //     .collect::<Vec<String>>();
+
+    // let port = port.first().unwrap();
+
+    // println!("port stdout:");
+    // println!("{}", port);
+    // format!("{}:{}", tank_id, port)
+    tank_id
+}
+
+pub fn run_game(args: &[String]) {
+    let game_url = args.join("");
+    let tank_ids = args
+        .iter()
+        .enumerate()
+        .map(|(i, url)| run_tank(url, &game_url, i))
+        .collect::<Vec<String>>();
+
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
