ac93d8d0e4ec083632f1f683a10a80708389aacd
Derrick Liu <derrick56007@hotmail.com>
2022-08-31 06:38:31 +0000

    save transforms

--- simulator/src/s_save_commands.rs
+++ simulator/src/s_save_commands.rs
@@ -1,15 +1,21 @@
 use bevy::prelude::*;
 use std::{fs::OpenOptions, io::Write};
 
-use crate::{c_command::CommandSource, TickState};
+use crate::{c_command::CommandSource, TickState, c_tank::*};
 use bevy::app::AppExit;
 
 pub fn save_commands(
     mut state: ResMut<TickState>,
     mut exit: EventWriter<AppExit>,
     query: Query<&CommandSource>,
+    tanks: Query<&Transform, With<Tank>>,
+    radars: Query<&Transform, With<Radar>>,
+    guns: Query<&Transform, With<Gun>>,
 ) {
-    for command_receiver in &query {
+    let tanks: Vec<&Transform> = tanks.iter().collect();
+    let radars: Vec<&Transform> = radars.iter().collect();
+    let guns: Vec<&Transform> = guns.iter().collect();
+    for (i, command_receiver) in query.iter().enumerate() {
         let grouped_commands = command_receiver.queue[0];
 
         // println!("save_commands {:?}", grouped_commands);
@@ -18,7 +24,21 @@ pub fn save_commands(
             .append(true)
             .open("./sim.txt")
             .expect("Unable to open file");
-        f.write_all(format!("{}\n", grouped_commands).to_string().as_bytes())
+        f.write_all(
+            format!(
+                "{}|{}{}{}{}{}{}{}{}{}{}\n",
+                 grouped_commands, 
+                 tanks[i].translation.x,
+                 tanks[i].translation.y,
+                 radars[i].rotation.x,
+                 radars[i].rotation.y,
+                 radars[i].rotation.z,
+                 radars[i].rotation.w,
+                 guns[i].rotation.x,
+                 guns[i].rotation.y,
+                 guns[i].rotation.z,
+                 guns[i].rotation.w,
+                ).to_string().as_bytes())
             .expect("Unable to write data");
 
         // println!("commands remaining {:?}", command_receiver.queue);
