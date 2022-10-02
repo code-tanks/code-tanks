15851339619f82bb5ef1145e998fc336242718e9
Derrick Liu <derrick56007@hotmail.com>
2022-10-02 06:57:51 +0000

    test

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -27,6 +27,12 @@ fn cli() -> Command {
                 .arg_required_else_help(true)
                 .arg(arg!(<TANK_ID> "The tank id to download")),
         )
+        .subcommand(
+            Command::new("run")
+                .about("Run simulation with tank ids")
+                .arg_required_else_help(true)
+                .arg(arg!(<TANK_ID> "The tank ids to run")),
+        )
 }
 
 fn upload(path: &str, extension: &str) {
@@ -50,15 +56,49 @@ fn upload(path: &str, extension: &str) {
     if result_raw != "" {
         println!("Tank Id: {}", result_raw.to_string().green());
     }
-    // println!("");
-    // println!("stderr:");
-    // println!("{}", err_raw.to_string());
-    // println!("");
 }
 
 fn get_logs(tank_id: &str) {
     let output_raw = std::process::Command::new("curl")
         .arg(format!("http://localhost:8089/log/{}", tank_id))
+        .output()
+        .expect("failed to communicate with CodeTanks server");
+
+    let result_raw = String::from_utf8_lossy(&output_raw.stdout).to_string();
+    let err_raw = String::from_utf8_lossy(&output_raw.stderr).to_string();
+
+    if err_raw != "" {
+        println!("{}", err_raw);
+    }
+    if result_raw != "" {
+        println!("{}", result_raw);
+    }
+}
+
+fn get_raw(tank_id: &str) {
+    let output_raw = std::process::Command::new("curl")
+        .arg(format!("http://localhost:8089/raw/{}", tank_id))
+        .output()
+        .expect("failed to communicate with CodeTanks server");
+
+    let result_raw = String::from_utf8_lossy(&output_raw.stdout).to_string();
+    let err_raw = String::from_utf8_lossy(&output_raw.stderr).to_string();
+
+    if err_raw != "" {
+        println!("{}", err_raw);
+    }
+    if result_raw != "" {
+        println!("{}", result_raw);
+    }
+}
+
+fn run_sim(tank_ids: Vec<String>) {
+    let output_raw = std::process::Command::new("curl")
+        .arg("-d")
+        .arg(format!("{}", tank_ids.join(" ")))
+        .arg("-X")
+        .arg("POST")
+        .arg("http://localhost:8089/run")
         .output()
         .expect("failed to communicate with CodeTanks server");
 
@@ -105,13 +145,17 @@ fn main() {
                     .expect("required"),
             );
         }
-        Some(("raw", _sub_matches)) => {
-            // println!(
-            //     "logs for {}",
-            //     sub_matches
-            //         .get_one::<String>("TANK_ID")
-            //         .expect("required")
-            // );
+        Some(("raw", sub_matches)) => {
+            get_raw(sub_matches.get_one::<String>("TANK_ID").expect("required"));
+        }
+        Some(("run", sub_matches)) => {
+            run_sim(
+                sub_matches
+                    .get_many::<String>("TANK_ID")
+                    .expect("required")
+                    .map(|f| f.to_string())
+                    .collect(),
+            );
         }
         _ => unreachable!(), // If all subcommands are defined above, anything else is unreachabe!()
     }
