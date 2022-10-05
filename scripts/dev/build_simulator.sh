ab15b8be1a49e7c3c75155d3b5cf12f80f728980
Derrick Liu <derrick56007@hotmail.com>
2022-10-05 18:17:10 +0000

    test

--- scripts/dev/build_simulator.sh
+++ scripts/dev/build_simulator.sh
@@ -2,5 +2,6 @@
 
 git pull
 
-docker build -t ghcr.io/code-tanks/code-tanks-simulator:latest -f worker_simulator/Dockerfile .
+export DOCKER_BUILDKIT=1
+docker build -t ghcr.io/code-tanks/code-tanks-simulator:latest -f worker_simulator/worker_simulator.Dockerfile .
 docker compose up -d --force-recreate --remove-orphans simulator
