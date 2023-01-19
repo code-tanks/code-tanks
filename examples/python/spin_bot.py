d5d83fb10d3e9df551d4f9daffec751c2d454dec
code-tanks <112593882+code-tanks@users.noreply.github.com>
2023-01-19 02:55:02 +0000

    examples

---
+++ examples/python/spin_bot.py
@@ -0,0 +1,22 @@
+from codetanks import *
+
+
+class MyTank(BaseTank):
+    def __init__(self):
+        super().__init__()
+
+        self.direction = 0
+
+        print('Running my spinning tank!')
+
+    def run(self):
+        if self.direction % 2 == 0:
+            self.commands.append(commands.MOVE_BACKWARD | commands.ROTATE_TANK_COUNTER_CLOCKWISE | commands.FIRE)
+        else:
+            self.commands.append(commands.MOVE_FORWARD | commands.ROTATE_TANK_CLOCKWISE | commands.FIRE)
+
+    def on_event(self, event):
+        self.direction = self.direction + 1
+
+def create_tank():
+    return MyTank()
---
+++ examples/rust/spin_bot.rs
@@ -0,0 +1,24 @@
+use ct_api::*;
+use serde_json::Value;
+
+pub struct MyTank {
+    i: u32
+}
+
+impl Tank for MyTank {
+    fn run(&mut self, commands: &mut Vec<CCommand>) {
+        if i % 2 == 0 {
+            commands.push(CCommands::MOVE_FORWARD | CCommands::ROTATE_TANK_CLOCKWISE | CCommands::FIRE);
+        } else {
+            commands.push(CCommands::MOVE_BACKWARD | CCommands::ROTATE_TANK_COUNTER_CLOCKWISE | CCommands::FIRE);
+        }
+    }
+
+    fn on_event(&mut self, commands: &mut Vec<CCommand>, event: &Value) {
+        self.i += 1;
+    }
+}
+
+pub fn create_tank() -> MyTank {
+    MyTank {}
+}
