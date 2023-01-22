ed5fcdc25c8a38aed0d6f240254e846af0847c54
code-tanks <112593882+code-tanks@users.noreply.github.com>
2023-01-22 00:55:10 +0000

    refactoring

--- simulator/src/c_client.rs
+++ simulator/src/c_client.rs
@@ -21,88 +21,36 @@ pub struct DockerClient {
 
 impl ClientTrait for DockerClient {
     fn request_commands(&mut self) -> Vec<CCommand> {
-        // println!(
-        //     r#"curl {}:8080/request_commands | jq --raw-output '.[]'"#,
-        //     self.tank_id
-        // );
-        let output_raw = Command::new("bash")
+        let output = Command::new("bash")
             .arg("-c")
             .arg(format!(
                 r#"curl {}:8080/request_commands | jq --raw-output '.[]'"#,
                 self.tank_id,
             ))
-            // .arg("ocypod:8023/queue/build/job")
             .output()
             .expect("failed to communicate with tank");
 
-        // let result_raw = String::from_utf8_lossy(&output_raw.stdout);
-        // let err_raw = String::from_utf8_lossy(&output_raw.stderr);
+        let result_raw = String::from_utf8_lossy(&output.stdout);
+        let _err_raw = String::from_utf8_lossy(&output.stderr);
 
-        // let mut res: Vec<CCommand> = vec![];
-        let result_raw = String::from_utf8_lossy(&output_raw.stdout);
-        let _err_raw = String::from_utf8_lossy(&output_raw.stderr);
-
-        // println!("out: {}", result_raw.to_string());
-        // println!("err: {}", err_raw.to_string() != "");
-
-        // let successful = err_raw.to_string() == "";
-
-        // println!("tank_id={}, successful={}", self.tank_id, successful);
-        // println!("stdout:");
-        // println!("{}", result_raw.to_string());
-        // println!("");
-        // println!("stderr:");
-        // println!("{}", err_raw.to_string());
-        // println!("");
-
-        // if err_raw.to_string() == "" {
-        // res =
-        result_raw
-            .to_string()
-            .split('\n')
-            .map(|f| f.to_string())
-            .filter(|f| !f.is_empty())
-            .filter_map(|f| f.parse::<CCommand>().ok())
-            .collect::<Vec<CCommand>>()
-        //         ;
-        // } else {
-        //     println!("stderr:");
-        //     println!("{}", err_raw.to_string());
-        //     println!("");
-        // }
-
-        // res
+        parse_commands(result_raw.to_string())
     }
 
     fn request_commands_by_event(&mut self, event: &Event) -> Vec<CCommand> {
-        let output_raw = Command::new("bash")
+        let output = Command::new("bash")
             .arg("-c")
             .arg(format!(
                 r#"curl -d '{}' -X POST {}:8080/request_commands_by_event | jq --raw-output '.[]'"#,
                 serde_json::to_string(event).unwrap(),
                 self.tank_id,
             ))
-            // .arg("ocypod:8023/queue/build/job")
             .output()
             .expect("failed to communicate with ocypod");
 
-        let result_raw = String::from_utf8_lossy(&output_raw.stdout);
-        let _err_raw = String::from_utf8_lossy(&output_raw.stderr);
+        let result_raw = String::from_utf8_lossy(&output.stdout);
+        let _err_raw = String::from_utf8_lossy(&output.stderr);
 
-        // let mut res: Vec<CCommand> = vec![];
-
-        // if err_raw.to_string() == "" {
-        //     res =
-        // println!("request event: \n {}", result_raw.to_string());
-        // println!("request event2: \n {}", err_raw.to_string());
-
-        result_raw
-            .to_string()
-            .split('\n')
-            .map(|f| f.to_string())
-            .filter(|f| !f.is_empty())
-            .filter_map(|f| f.parse::<CCommand>().ok())
-            .collect::<Vec<CCommand>>()
+        parse_commands(result_raw.to_string())
     }
 }
 
@@ -122,4 +70,13 @@ impl ClientTrait for ReaderClient {
     fn request_commands_by_event(&mut self, _event: &Event) -> Vec<CCommand> {
         self.request_commands()
     }
+}
+
+fn parse_commands(commands_string: String) -> Vec<CCommand> {
+    commands_string
+        .split('\n')
+        .map(|f| f.to_string())
+        .filter(|f| !f.is_empty())
+        .filter_map(|f| f.parse::<CCommand>().ok())
+        .collect::<Vec<CCommand>>()
 }
