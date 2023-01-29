d76ad5eab7c09440f3cc7569be02072944cb73a8
Derrick Liu <derrick56007@hotmail.com>
2023-01-28 16:09:19 -0800

    remove unused

--- scripts/dev/build_simulator.sh
+++ scripts/dev/build_simulator.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-git pull
-
 docker build -t ghcr.io/code-tanks/code-tanks-simulator:latest -f worker_simulator/Dockerfile .
 docker compose up -d --force-recreate --remove-orphans simulator
--- scripts/dev/up_db.sh
+++ scripts/dev/up_db.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-git pull
 docker compose up -d db ocypod ocypod-redis registry pgAdmin
