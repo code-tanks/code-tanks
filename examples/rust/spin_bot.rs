643daee78ceda59b9223891cb466eacdcb3ab939
code-tanks <112593882+code-tanks@users.noreply.github.com>
2023-02-02 01:22:56 +0000

    fix

--- examples/rust/spin_bot.rs
+++ examples/rust/spin_bot.rs
@@ -6,15 +6,15 @@ pub struct MyTank {
 }
 
 impl Tank for MyTank {
-    fn run(&mut self, commands: &mut Vec<CCommand>) {
+    fn run(&mut self, commands: &mut Vec<Command>) {
         if self.i % 2 == 0 {
-            commands.push(CCommands::MOVE_FORWARD | CCommands::ROTATE_TANK_CLOCKWISE | CCommands::FIRE);
+            commands.push(Commands::MOVE_FORWARD | Commands::ROTATE_TANK_CLOCKWISE | Commands::FIRE);
         } else {
-            commands.push(CCommands::MOVE_BACKWARD | CCommands::ROTATE_TANK_COUNTER_CLOCKWISE | CCommands::FIRE);
+            commands.push(Commands::MOVE_BACKWARD | Commands::ROTATE_TANK_COUNTER_CLOCKWISE | Commands::FIRE);
         }
     }
 
-    fn on_event(&mut self, commands: &mut Vec<CCommand>, event: &Value) {
+    fn on_event(&mut self, commands: &mut Vec<Command>, event: &Value) {
         self.i += 1;
     }
 }
