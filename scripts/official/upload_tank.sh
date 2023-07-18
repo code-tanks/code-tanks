b956daf7dc81d68749385a3a2a3d947bfb964d46
Derrick Liu <derrick56007@hotmail.com>
2023-07-17 21:26:36 -0700

    fix data upload not preserving linebreaks

--- scripts/official/upload_tank.sh
+++ scripts/official/upload_tank.sh
@@ -3,5 +3,5 @@
 export $(xargs < ./scripts/.env)
 
 # curl --data-binary "@$1" -H 'Content-Type: text/plain' -X POST $HOST/upload/${1##*.} --http1.1
-docker run --network host --rm -it -v "$(dirname $(readlink -f $1)):/work" curlimages/curl:7.80.0 -d@/work/${1##*/} -H 'Content-Type: text/plain' -X POST $HOST/upload/${1##*.} --http1.1
+docker run --network host --rm -it -v "$(dirname $(readlink -f $1)):/work" curlimages/curl:7.80.0 --data-binary @/work/${1##*/} -H 'Content-Type: text/plain' $HOST/upload/${1##*.} --http1.1
 echo ""
