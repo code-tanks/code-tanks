f97a5f1461b31c1d4d8c91bcdf9d14ee4f93927b
Derrick Liu <derrick56007@hotmail.com>
2023-07-24 11:16:40 -0700

    remove interactive

--- scripts/get_build_log.sh
+++ scripts/get_build_log.sh
@@ -3,5 +3,5 @@
 . ./scripts/helper/setup_host.sh
 
 # curl $HOST/log/$1 --http1.1
-docker run --network host --rm -it curlimages/curl:7.80.0 -s $HOST/log/$1 --http1.1
+docker run --network host --rm curlimages/curl:7.80.0 -s $HOST/log/$1 --http1.1
 echo ""
--- scripts/get_sim_log.sh
+++ scripts/get_sim_log.sh
@@ -3,5 +3,5 @@
 . ./scripts/helper/setup_host.sh
 
 # curl $HOST/sim_log/$1 --http1.1
-docker run --network host --rm -it curlimages/curl:7.80.0 -s $HOST/sim_log/$1 --http1.1
+docker run --network host --rm curlimages/curl:7.80.0 -s $HOST/sim_log/$1 --http1.1
 echo ""
