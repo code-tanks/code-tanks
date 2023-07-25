0ac6334299aeb9a50827c76da0df4b8ebcef208f
Derrick Liu <derrick56007@hotmail.com>
2023-07-24 19:24:11 -0700

    remove print

--- scripts/dev/no-docker/get_raw.sh
+++ scripts/dev/no-docker/get_raw.sh
@@ -2,6 +2,6 @@
 
 . ./scripts/helper/setup_host.sh
 
-curl $HOST/raw/$1 --http1.1
+curl -s $HOST/raw/$1 --http1.1
 # docker run --network host --rm curlimages/curl:7.80.0 -s $HOST/raw/$1 --http1.1
 echo ""
--- scripts/dev/no-docker/upload_tank.sh
+++ scripts/dev/no-docker/upload_tank.sh
@@ -2,6 +2,6 @@
 
 . ./scripts/helper/setup_host.sh
 
-curl --data-binary "@$1" -H 'Content-Type: text/plain' -X POST $HOST/upload/${1##*.} --http1.1
+curl -s --data-binary "@$1" -H 'Content-Type: text/plain' -X POST $HOST/upload/${1##*.} --http1.1
 # docker run --network host --rm -v "$(dirname $(readlink -f $1)):/work" curlimages/curl:7.80.0 -s --data-binary @/work/${1##*/} -H 'Content-Type: text/plain' $HOST/upload/${1##*.} --http1.1
 echo ""
