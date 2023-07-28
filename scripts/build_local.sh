407073564a27b115fded76220458d8770d70bd26
Derrick Liu <derrick56007@hotmail.com>
2023-07-28 13:32:20 -0700

    use local hash

--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -2,4 +2,7 @@
 
 export $(xargs < ./scripts/.env)
 
-docker build -f ./scripts/Dockerfiles/${1##*.}.Dockerfile --build-arg url=$1 -t ${1##*/} .
+tank_hash=$(bash -c 'echo -n "$(cat $1)0" | sha256sum - | cut -d" " -f1' -- "$1" | cut -c 1-7)
+
+docker build -f ./scripts/Dockerfiles/${1##*.}.Dockerfile --build-arg url=$1 -t $tank_hash .
+echo $tank_hash
