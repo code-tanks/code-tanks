7d28273dc0a67d93c0f0ea6481f783c825ee4ff9
Derrick Liu <derrick56007@hotmail.com>
2023-08-01 18:39:36 -0700

    remove unused arg import

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export $(xargs < ./scripts/.env)
-
 tank_hash=$(bash -c 'sha256sum $1 | cut -d" " -f1' -- "$1" | cut -c 1-7)
 
 docker build -f ./scripts/Dockerfiles/${1##*.}.Dockerfile --build-arg url=$1 -t $tank_hash .
